9ac98624599122f172dc19ba820410c8e9c5d94e
Francisco Villa <francisco.villa.gonzalez@gmail.com>
2020-06-14 20:52:30 +0200

    presentacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
-console.log("hi");
+//
+const text = document.querySelector(".main__item-description>p").innerHTML;
+document.querySelector(".main__item-description > p").innerHTML = "";
+const e_text = document.querySelector(".main__item-description > p");
+
+const text2 = document.querySelector(".main__item-description p:last-child ")
+  .innerHTML;
+document.querySelector(".main__item-description  p:last-child").innerHTML = "";
+const e_text2 = document.querySelector(".main__item-description  p:last-child");
+
+setTimeout(() => {
+  const t3 = typer(e_text)
+    .line(" " + text, { totalTime: 25 })
+    .cursor({ block: false, color: "white" });
+  setTimeout(() => {
+    t3.kill();
+    const t4 = typer(e_text2)
+      .line(" " + text2, { totalTime: 25 })
+      .cursor({ block: false, color: "white" });
+  }, 4000);
+});
